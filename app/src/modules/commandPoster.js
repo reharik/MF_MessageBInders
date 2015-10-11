a7d3c418696f3aa5ccdd8f9c0077f26cf951873d
Raif Harik <reharik@gmail.com>
2015-10-11 10:27:54 -0500

    had to update module

--- app/src/modules/commandPoster.js
+++ app/src/modules/commandPoster.js
@@ -8,9 +8,9 @@ module.exports = function(eventmodels, eventstore){
         // fortify commands with metadata like date and user
         command.createDate = new Date();
         var appendData = { expectedVersion: -2};
-        appendData.events = [new core.EventData( commandName,
+        appendData.events = [eventmodels.eventData( commandName,
                                             { data:command },
                                             {eventName:commandName, continuationId:uuid.v4(), streamType:'command'})];
-        return core.appendToStreamPromise('commands', appendData);
+        return eventstore.appendToStreamPromise('commands', appendData);
     }
 };
