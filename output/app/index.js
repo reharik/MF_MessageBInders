d3c5dfff731acfa40134c34fb14e69dea015af66
Raif Harik <reharik@gmail.com>
2015-10-17 15:06:05 -0500

    kill me

--- output/app/index.js
+++ output/app/index.js
@@ -2,7 +2,7 @@
  * Created by reharik on 8/13/15.
  */
 
-var container = require('./registry');
+var registry = require('./registry');
 var extend = require('extend');
 
 module.exports = function index(_options) {
