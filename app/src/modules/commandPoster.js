9c4ac3b39dda4ebac877d769874ceab94c857ef3
Raif Harik <reharik@gmail.com>
2016-11-09 12:56:25 -0600

    passing in the contId rather then gening it

--- app/src/modules/commandPoster.js
+++ app/src/modules/commandPoster.js
@@ -4,19 +4,18 @@
 "use strict";
 
 module.exports = function(appfuncs, eventstore, uuid){
-    return function(command, commandName){
+    return function(command, commandName, continuationId){
         // fortify commands with metadata like date and user
         command.createDate = new Date();
         var appendData = { expectedVersion: -2};
-
         appendData.events = [
             appfuncs.eventFunctions.outGoingEvent({
                 eventName:commandName,
                 data: command,
-                metadata:{eventName:commandName, continuationId:uuid.v4(), streamType:'command'}
+                metadata:{eventName:commandName, continuationId, streamType:'command'}
             })
         ];
 
-        return eventstore.appendToStreamPromise('commands', appendData);
+        eventstore.appendToStreamPromise('commands', appendData);
     }
 };
