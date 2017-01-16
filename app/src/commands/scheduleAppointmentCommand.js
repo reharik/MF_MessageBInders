2272097d1d71e501744ac2620b8565310ae8f9a0
Raif Harik <reharik@gmail.com>
2017-01-16 11:16:02 -0600

    added entity name to appt

--- app/src/commands/scheduleAppointmentCommand.js
+++ app/src/commands/scheduleAppointmentCommand.js
@@ -5,19 +5,23 @@ module.exports = function(invariant) {
     endTime,
     trainer,
     clients,
-    notes }) {
+    notes,
+    entityName}) {
     invariant(appointmentType, 'scheduleAppointment requires that you pass the appointmentType');
     invariant(date, 'scheduleAppointment requires that you pass the appointment date');
     invariant(startTime, 'scheduleAppointment requires that you pass the appointment start time');
     invariant(endTime, 'scheduleAppointment requires that you pass the trainer');
     invariant(clients && clients.length > 0, 'scheduleAppointment requires that you pass at lease 1 client');
+    invariant(entityName, `scheduleAppointment requires that you pass the 
+      enitityName since it's a date but the date prop is utc` );
     return {appointmentType,
       date,
       startTime,
       endTime,
       trainer,
       clients,
-      notes
+      notes,
+      entityName
     };
   }
 };
