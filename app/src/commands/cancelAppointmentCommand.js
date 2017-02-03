17c80b420958841621e2118a01afec1f05c8c981
Raif Harik <reharik@gmail.com>
2017-02-03 17:28:16 -0600

    working on cancelappointment

--- app/src/commands/cancelAppointmentCommand.js
+++ app/src/commands/cancelAppointmentCommand.js
@@ -1,14 +1,12 @@
 module.exports = function(invariant) {
   return function ({
-      messageName,
       appointmentId,
       entityName}) {
     
-    invariant(appointmentId, `${messageName} requires that you pass the AppointmentId`);
-    invariant(entityName, `${messageName} requires that you pass the 
+    invariant(appointmentId, `Cancel appointment requires that you pass the AppointmentId`);
+    invariant(entityName, `Cancel appointment requires that you pass the 
       enitityName since it's a date but the date prop is utc` );
     return {
-      messageName,
       appointmentId,
       entityName};
   }
