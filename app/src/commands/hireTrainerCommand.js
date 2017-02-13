bfebbd9323b8840c61f5bf2e0cc4957d69467dc7
Raif Harik <reharik@gmail.com>
2017-02-13 10:06:03 -0600

    changing hiretrainer command

--- app/src/commands/hireTrainerCommand.js
+++ app/src/commands/hireTrainerCommand.js
@@ -1,47 +1,11 @@
 module.exports = function(invariant) {
-  return function ({birthDate,
-                   color,
-                   firstName,
-                   lastName,
-                   secondaryPhone,
-                   mobilePhone,
-                   email,
-                   password,
-                   role,
-                   street1,
-                   street2,
-                   city,
-                   state,
-                   zipCode,
-                   clients}) {
-    invariant(firstName, 'hireTrainer requires that you pass the trainers first name');
-    invariant(lastName, 'hireTrainer requires that you pass the trainers last name');
-    invariant(email, 'hireTrainer requires that you pass the trainers email');
-    invariant(mobilePhone, 'hireTrainer requires that you pass the trainers mobilePhone');
-    invariant(password, 'hireTrainer requires that you pass the trainers password');
-    invariant(role, 'hireTrainer requires that you pass the trainers role');
-    return {
-      credentials: {
-        password,
-        role
-      },
-      contact: {
-        firstName,
-        lastName,
-        secondaryPhone,
-        mobilePhone,
-        email,
-        address: {
-          street1,
-          street2,
-          city,
-          state,
-          zipCode
-        }
-      },
-      birthDate,
-      color,
-      clients
-    };
-  }
+  return function ({trainer}) {
+    invariant(trainer.contact.firstName, 'hireTrainer requires that you pass the trainers first name');
+    invariant(trainer.contact.lastName, 'hireTrainer requires that you pass the trainers last name');
+    invariant(trainer.contact.email, 'hireTrainer requires that you pass the trainers email');
+    invariant(trainer.contact.mobilePhone, 'hireTrainer requires that you pass the trainers mobilePhone');
+    invariant(trainer.credentials.password, 'hireTrainer requires that you pass the trainers password');
+    invariant(trainer.credentialsrole, 'hireTrainer requires that you pass the trainers role');
+    return trainer;
+  };
 };
