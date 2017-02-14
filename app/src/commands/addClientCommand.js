00a0f6fd458143f25cfb53ff90e7015f20e9ecc1
Raif Harik <reharik@gmail.com>
2017-02-13 19:07:15 -0600

    having addclient pass the client through

--- app/src/commands/addClientCommand.js
+++ app/src/commands/addClientCommand.js
@@ -1,44 +1,10 @@
 module.exports = function(invariant) {
-  return function ({firstName,
-                   lastName,
-                   secondaryPhone,
-                   mobilePhone,
-                   email,
-                   street1,
-                   street2,
-                   city,
-                   state,
-                   zipCode, 
-                  id,
-                  source,
-                  sourceNotes,
-                  birthDate,
-                  startDate}) {
-    invariant(firstName, 'addClient requires that you pass the clients first name');
-    invariant(lastName, 'addClient requires that you pass the clients last name');
-    invariant(email, 'addClient requires that you pass the clients email');
-    invariant(mobilePhone, 'addClient requires that you pass the clients mobilePhone');
-    invariant(startDate, 'addClient requires that you pass the clients startDate');
-    return {
-      id,
-      source,
-      sourceNotes,
-      birthDate,
-      startDate,
-      contact: {
-        firstName,
-        lastName,
-        secondaryPhone,
-        mobilePhone,
-        email,
-        address: {
-          street1,
-          street2,
-          city,
-          state,
-          zipCode
-        }
-      }
-    };
+  return function (client) {
+    invariant(client.contact.firstName, 'addClient requires that you pass the clients first name');
+    invariant(client.contact.lastName, 'addClient requires that you pass the clients last name');
+    invariant(client.contact.email, 'addClient requires that you pass the clients email');
+    invariant(client.contact.mobilePhone, 'addClient requires that you pass the clients mobilePhone');
+    invariant(client.startDate, 'addClient requires that you pass the clients startDate');
+    return client
   }
 };
