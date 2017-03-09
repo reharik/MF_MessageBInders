ed2889506a3d1c3c86904682a2fc45b1d2f90cbd
Raif Harik <reharik@gmail.com>
2017-03-09 15:46:28 -0600

    fixing commands to command

--- app/src/modules/commandPoster.js
+++ app/src/modules/commandPoster.js
@@ -3,6 +3,6 @@ module.exports = function(appfuncs, eventstore, uuid){
         // fortify commands with metadata like date and user
       command.createDate = new Date();
       var event = eventstore.createJsonEventData(uuid.v4(), command, {eventName:commandName, continuationId, streamType:'command'}, commandName);
-      await eventstore.gesConnection.appendToStream('commands', eventstore.expectedVersion.any, event, eventstore.credentials);
+      await eventstore.gesConnection.appendToStream('command', eventstore.expectedVersion.any, event, eventstore.credentials);
     }
 };
