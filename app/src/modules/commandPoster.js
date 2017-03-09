e8682e80f6a1a7070bf2a38f95efead28b0083fd
Raif Harik <reharik@gmail.com>
2017-03-09 09:15:25 -0600

    forgot to declare async

--- app/src/modules/commandPoster.js
+++ app/src/modules/commandPoster.js
@@ -1,5 +1,5 @@
 module.exports = function(appfuncs, eventstore){
-    return function(command, commandName, continuationId) {
+    return async function(command, commandName, continuationId) {
         // fortify commands with metadata like date and user
       command.createDate = new Date();
       var event = eventstore.createJsonEventData(uuid.v4(), command, {eventName:commandName, continuationId, streamType:'command'}, commandName);
